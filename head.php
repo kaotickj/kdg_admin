a441db71c6a9b4654846de7974460d0ec070349a
Johnny Watts <58899228+kaotickj@users.noreply.github.com>
2022-04-13 00:17:50 -0400

    Update head.php

--- head.php
+++ head.php
@@ -55,11 +55,7 @@ if (!empty ($_GET['page'])) {
 				if ($no_index == 0)echo '<meta name="robots" content="index, follow" />';
 		?>
 				<meta property="og:image" content="<?php echo $img;?>" />
-				<meta property="og:image:type" content="image/jpg" />
-				<meta property="og:image:width" content="561" />
-				<meta property="og:image:height" content="350" />
 				<meta property="og:title" content="<?php echo $title;?>" />
-				<meta property="og:type" content="website" />
 				<meta property="og:url" content="<?php echo $siteURL;?><?php echo $page;?>" />
 				<meta property="og:site_name" content="<?php echo $title;?>" />
 				<meta property="og:description" content="<?php echo $desc;?>" />
@@ -74,9 +70,18 @@ if (!empty ($_GET['page'])) {
 				<meta property="og:image" content="assets/img/bg.jpg">
 				<meta name="description" content="Every cup of our quality artisan coffee starts with locally sourced, hand picked ingredients. Once you try it, our coffee will be a blissful addition to your everyday morning routine - we guarantee it!">
 				<meta name="twitter:description" content="Every cup of our quality artisan coffee starts with locally sourced, hand picked ingredients. Once you try it, our coffee will be a blissful addition to your everyday morning routine - we guarantee it!">
+				<meta name="robots" content="index, follow" />
+				<meta property="og:image" content="assets/img/bg.jpg" />
+				<meta property="og:title" content="Hernandez Brothers" />
+				<meta property="og:url" content="<?php echo $siteURL;?>" />
+				<meta property="og:site_name" content="Hernandez Brothers" />
+				<meta property="og:description" content="Every cup of our quality artisan coffee starts with locally sourced, hand picked ingredients. Once you try it, our coffee will be a blissful addition to your everyday morning routine - we guarantee it!" />
 <?php
 }
 ?>
+				<meta property="og:image:type" content="image/jpg" />
+				<meta property="og:image:width" content="561" />
+				<meta property="og:image:height" content="350" />
 				<meta property="og:type" content="website">
 				<meta name="twitter:card" content="summary_large_image">
 				<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
