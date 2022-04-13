d6784966f4ee688e86a53099648b3fe2d05bb9a7
Johnny Watts <58899228+kaotickj@users.noreply.github.com>
2022-04-12 23:53:51 -0400

    Update robots.php

--- robots.php
+++ robots.php
@@ -1,73 +1,39 @@
 <?php
 	include 'config.php';
-			$conn = new mysqli($servername, $username, $password);		
-				if ($conn->connect_error) {
-				die('' . $db_error .'');
-			} 
-		
+	$conn = new mysqli($servername, $username, $password);		
+		if ($conn->connect_error) {
+		die('' . $db_error .'');
+	} 
 
-		$bot_ip = $_SERVER['REMOTE_ADDR'];
-		$usrAgnt = $_SERVER['HTTP_USER_AGENT'];
-		$whereFrom = $_SERVER['REQUEST_URI'];
-		$created = date(DATE_W3C);
-        $remotehost = @getHostByAddr($visitor_ip);
-		if(isset($_SERVER['HTTP_REFERER'])) {
-        $referer = $_SERVER['HTTP_REFERER'];
-		} else{
+	$bot_ip = $_SERVER['REMOTE_ADDR'];
+	$usrAgnt = $_SERVER['HTTP_USER_AGENT'];
+	$whereFrom = $_SERVER['REQUEST_URI'];
+	$created = date(DATE_W3C);
+	$remotehost = @getHostByAddr($visitor_ip);
+	if(isset($_SERVER['HTTP_REFERER'])) {
+		$referer = $_SERVER['HTTP_REFERER'];
+	} else{
 		$referer = 'Not Set';	
-		}		
-
+	}		
 //  <============== Check if bot has been logged ==============>	
-
 	mysqli_select_db($conn,$dbname);		
-			$sql='SELECT `ip-addy` FROM `bot-logs` WHERE `ip-addy`="'.$bot_ip.'"'; 
-			$rs = mysqli_query($conn, $sql);
-					if (mysqli_num_rows($rs) > 0) { 
-			
-    if(! $rs ) {
-        die('Could not get data: ' . mysql_error());
-   }
-
-			while($row = mysqli_fetch_assoc($rs)) { 	
-						
-				//Bot has already been logged. Do nothing and show robots.txt
-				print file_get_contents("robots.txt");			
-			}
-
-	
-		} else {
-			// Log this bot ip and show robots.txt
-			
-$sql= "INSERT INTO `bot-logs` (`ip-addy`, `logged-from`, `usr-agnt`, `created_date`, `referer`, `remotehost`) VALUES ('$bot_ip', '$whereFrom', '$usrAgnt', '$created', '$referer', '$remotehost')";
-
-
-if (mysqli_query($conn, $sql)) {
-			print file_get_contents("robots.txt"); 	
-} 			
-			
-			
-
-		}	
-	
-/*		
-
-
-		$conn = new mysqli($servername, $username, $password, $dbname);
-			$sql='select * from `bot-logs`'; 
-			$query = mysqli_query($conn, $sql);
-
-		if (mysqli_num_rows($query) > 0) { 
-
-			while($row = mysqli_fetch_assoc($query)) { 
-		
-				if ($row['ip-addy']== $bot_ip) file_put_contents($file2, $deny. "\n", FILE_APPEND | LOCK_EX); 
-		
+	$sql='SELECT `ip-addy` FROM `bot-logs` WHERE `ip-addy`="'.$bot_ip.'"'; 
+	$rs = mysqli_query($conn, $sql);
+	if (mysqli_num_rows($rs) > 0) { 
+		if(! $rs ) {
+			die('Could not get data: ' . mysql_error());
 		}
+		while($row = mysqli_fetch_assoc($rs)) { 	
+					
+			//Bot has already been logged. Do nothing and show robots.txt
+			print file_get_contents("robots.txt");			
+		}
+	} else {
+		// Log this bot ip and show robots.txt
+		$sql= "INSERT INTO `bot-logs` (`ip-addy`, `logged-from`, `usr-agnt`, `created_date`, `referer`, `remotehost`) VALUES ('$bot_ip', '$whereFrom', '$usrAgnt', '$created', '$referer', '$remotehost')";
 
-
-
-
-
-*/	
-
+		if (mysqli_query($conn, $sql)) {
+			print file_get_contents("robots.txt"); 	
+		} 			
+	}	
 ?>
